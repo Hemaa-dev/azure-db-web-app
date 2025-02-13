eca6e20f42645fcd6ea7235b35530fb45a519095
Hemaa-dev <hemaa2107@gmail.com>
2025-02-13 12:17:40 +0530

    Add project files.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using azure_db_web_app.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace azure_db_web_app.Data
+{
+  
+    
+        public class AppDbContext : DbContext
+        {
+            public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+            public DbSet<Product> Product { get; set; } = default!;
+        }
+    
+}
---
+++ Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace azure_db_web_app.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
